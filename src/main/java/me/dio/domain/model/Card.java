ef632ec59e52c06d9ecd3daf29dd8e8dd06f6bea
Wagner Nogueira <wbnogueira3@gmail.com>
2023-09-07 15:15:24 -0300

    Implementação da camda de domínio

---
+++ src/main/java/me/dio/domain/model/Card.java
@@ -0,0 +1,43 @@
+package me.dio.domain.model;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity(name = "tb_card")
+public class Card {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(unique = true)
+    private String number;
+
+    @Column(name = "available_limit", scale = 13, precision = 2)
+    private BigDecimal limit;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public BigDecimal getLimit() {
+        return limit;
+    }
+
+    public void setLimit(BigDecimal limit) {
+        this.limit = limit;
+    }
+}
